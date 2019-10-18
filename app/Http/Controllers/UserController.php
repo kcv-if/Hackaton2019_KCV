c0e5cb161ba3a7a63573d2308ec75c8240adb0f0
rahandi <rahandinoor@gmail.com>
2019-10-18 16:50:40 +0700

    error handling player join id_room tidak ada

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,19 +14,23 @@ class UserController extends Controller
     }
 
     public function store(Request $request){
-        $user = new User();
-        $user->username = $request->username;
-        $user->save();
-
         $room = Room::all()->where('kode', $request->kode_room)->first();
-        $current_player = unserialize($room->player_id);
-        array_push($current_player, $user->id);
-        $room->player_id = serialize($current_player);
-        $room->save();
-        //broadcast
-        broadcast(new UserJoin($room))->toOthers();
-        $request->session()->put('user_id', $user->id);
-        return redirect()->route('room', ['id_room' => $room->kode]);
+        if($room) {
+            $user = new User();
+            $user->username = $request->username;
+            $user->save();
+            $current_player = unserialize($room->player_id);
+            array_push($current_player, $user->id);
+            $room->player_id = serialize($current_player);
+            $room->save();
+            //broadcast
+            broadcast(new UserJoin($room))->toOthers();
+            $request->session()->put('user_id', $user->id);
+            return redirect()->route('room', ['id_room' => $room->kode]);
+        }
+        else {
+            return 'id_room not found';
+        }
     }
 
     
