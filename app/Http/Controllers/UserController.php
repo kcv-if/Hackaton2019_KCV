92fa0a6e055072faa64ddfe1966aa83fb2fcd8b5
fandykun <fandy19061999@gmail.com>
2019-10-19 09:05:40 +0700

    error handling

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Events\UserJoin;
 use App\User;
 use App\Room;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
@@ -14,8 +15,9 @@ class UserController extends Controller
     }
 
     public function store(Request $request){
-        $room = Room::all()->where('kode', $request->kode_room)->first();
-        if($room) {
+        try {
+            $room = Room::all()->where('kode', $request->kode_room)->first();
+
             $user = new User();
             $user->username = $request->username;
             $user->save();
@@ -23,14 +25,18 @@ class UserController extends Controller
             array_push($current_player, $user->id);
             $room->player_id = serialize($current_player);
             $room->save();
+            
             //broadcast
             broadcast(new UserJoin($room))->toOthers();
             $request->session()->put('user_id', $user->id);
             return redirect()->route('room', ['id_room' => $room->kode]);
+            
+        }catch(\Exception $e) {
+            $eMessage = 'error: ' . $e->getMessage();
+            Log::emergency($eMessage);
+            return redirect()->back()->with('error', 'Terjadi kesalahan, ID Room tidak ditemukan!');
         }
-        else {
-            return 'id_room not found';
-        }
+        
     }
 
     
